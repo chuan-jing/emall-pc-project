1a671219e145e3104711a55a6dce0b7b9ecb2de9
chuan-jing <lcj010306@outlook.com>
2022-09-17 16:30:50 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import { createStore } from 'vuex'
 
+// vue2.0 创建仓库 new Vuex.Store({})
+// vue3.0 创建仓库 createStore({})
 export default createStore({
   state: {
+    // 存状态的,存数据的
   },
   getters: {
+    // 写计算属性的，vuex的计算属性
   },
   mutations: {
+    // 修改方法的，改数据函数
   },
   actions: {
+    // 拿数据的，请求数据函数
   },
   modules: {
+    // 分模块的
   }
 })
